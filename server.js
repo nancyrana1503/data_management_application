6945c6263cbfb9b7a75836674adaeb4ff1212cc4
Nancy Rana <nancyrana155@gmail.com>
2026-04-08 15:38:15 -0400

    add api index for vercel

--- server.js
+++ server.js
@@ -52,8 +52,4 @@ app.use("/", require("./routes/auth")(User));
 app.use("/", require("./routes/task")(Task));
 
 // Start
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
