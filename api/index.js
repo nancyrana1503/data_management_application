773b83595c4671e6fec2b64bddd68c886b595bdb
Nancy Rana <nancyrana155@gmail.com>
2026-04-08 10:59:00 -0400

    initial mi

--- api/index.js
+++ api/index.js
@@ -1,3 +1,61 @@
-module.exports = (req, res) => {
-  res.status(200).send("WORKING 🚀");
-};
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Sequelize = require("sequelize");
+const clientSessions = require("client-sessions");
+const serverless = require("serverless-http");
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+app.use(clientSessions({
+  cookieName: "session",
+  secret: process.env.SESSION_SECRET,
+  duration: 30 * 60 * 1000
+}));
+
+// MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// PostgreSQL
+const sequelize = new Sequelize(
+  process.env.PG_DB,
+  process.env.PG_USER,
+  process.env.PG_PASSWORD,
+  {
+    host: process.env.PG_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+sequelize.authenticate()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch(err => console.log(err));
+
+// Models
+const User = require("../models/user");
+const Task = require("../models/task")(sequelize);
+
+sequelize.sync();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Main API running 🚀");
+});
+
+app.use("/", require("../routes/auth")(User));
+app.use("/", require("../routes/task")(Task));
+
+module.exports = serverless(app);
